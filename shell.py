88338a316a88c511d5100181bab76fbfe223218d
Franck Labadille <franck@kernlog.net>
2011-01-28 00:21:57 +0100

    shell

--- shell.py
+++ shell.py
@@ -8,19 +8,19 @@ from pynancial.ui import *
 
 while True:
     print("What do you want to do ?\n\
-        1 Add to database\n\
-        2 Find in database\n\
-        3 Create URL\n\
-        9 Quit\n")
+        1 Add to database\n")
+#        2 Find in database\n\
+#        3 Create URL\n\
+#        9 Quit\n")
 
     userinteract = code.InteractiveConsole()
     response = userinteract.raw_input("tell me\n")
     if int(response) == 1:
         response = userinteract.raw_input("What do you want to add ?\n\
-            1 provider\n\
-            2 stock\n\
-            3 index\n\
-            4 format\n")
+            1 provider\n")
+#            2 stock\n\
+#            3 index\n\
+#            4 format\n")
 
         if int(response) == 1:
             addprovider()
